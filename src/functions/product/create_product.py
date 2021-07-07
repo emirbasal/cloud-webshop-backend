a9ae92ce4e4dd43eb8a2457b4d132454f2423e8f
hm-basal <basal@hm.edu>
2021-07-07 23:04:14 +0200

    Add images for product

--- src/functions/product/create_product.py
+++ src/functions/product/create_product.py
@@ -7,9 +7,11 @@ from src.functions.helper import decimalencoder
 from src.persistence import db_service
 
 
+#TODO: Input validation
 def create_product(event, context):
     data = json.loads(event['body'])
-    if 'name' not in data or 'description' not in data or 'currency' not in data or 'amount' not in data:
+    if 'name' not in data or 'description' not in data or 'currency' not in data or 'amount' not in data or \
+            'image' not in data:
         logging.error("Validation Failed. Attribute(s) are missing. Couldn't create the product.")
 
         response = {
@@ -29,6 +31,7 @@ def create_product(event, context):
         'description': data['description'],
         'amount': Decimal(str(data['amount'])),
         'currency': data['currency'],
+        'image': data['image'],
         'createdAt': timestamp,
         'updatedAt': timestamp,
     }
