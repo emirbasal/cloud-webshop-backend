d5ad21f6614e8a8798009e9a4194bfa2f7db0a92
hm-basal <basal@hm.edu>
2021-07-20 16:57:21 +0200

    Small changes in tests

--- src/test/test_calc_amount.py
+++ src/test/test_calc_amount.py
@@ -1,15 +1,14 @@
-import unittest
-from src.main.functions.order.create_order import calc_amount
+from unittest import TestCase, mock, main
+import os
 
 
 # Class for testing the calculated total amount of an order
-class TestCalcAmount(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        pass
+@mock.patch.dict(os.environ, {'FRONTEND_ORIGIN': 'FRONTEND_ORIGIN'})
+class TestCalcAmount(TestCase):
 
     def test_calc_normal_order(self):
+        print(os.environ["FRONTEND_ORIGIN"])
+
         order_items = [
             {
                 'amount': 100
@@ -21,6 +20,10 @@ class TestCalcAmount(unittest.TestCase):
                 'amount': 1
             }
         ]
+
+        # Import is here bc it will throw an error otherwise for not finding the env variables
+        from src.main.functions.order.create_order import calc_amount
+
         self.assertEqual(calc_amount(order_items), 351)
 
     def test_calc_all_zeros(self):
@@ -35,8 +38,11 @@ class TestCalcAmount(unittest.TestCase):
                 'amount': 0
             }
         ]
+        # Import is here bc it will throw an error otherwise for not finding the env variables
+        from src.main.functions.order.create_order import calc_amount
+
         self.assertEqual(calc_amount(order_items), 0)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
